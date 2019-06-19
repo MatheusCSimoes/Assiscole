ca2f423f820eb6fa12850c492db71f4d5f362f79
Miguel Angelo <masbicudo@gmail.com>
2019-06-19 20:42:00 -0300

    Correções para funcionar no MySQL 8

--- SQL_scripts/bd_views.sql
+++ SQL_scripts/bd_views.sql
@@ -16,15 +16,15 @@
 
 -- criando views
 DROP VIEW IF EXISTS inscricoes;
-DROP VIEW IF EXISTS estudante;
+DROP VIEW IF EXISTS estudantes;
 DROP VIEW IF EXISTS responsaveisEstudantes;
 DROP VIEW IF EXISTS listaPresenca;
-DROP VIEW IF EXISTS notificacoes;
+DROP VIEW IF EXISTS notificacoes2;
 
 CREATE VIEW inscricoes AS
     SELECT ins.fk_Estudante_CPF, ins.Nota, ins.Situacao FROM Inscricao_inscrito ins;
 
-CREATE VIEW estudante AS
+CREATE VIEW estudantes AS
     SELECT e.Nome, e.CPF FROM Estudante e;
 
 CREATE VIEW responsaveisEstudantes AS
@@ -33,7 +33,7 @@ CREATE VIEW responsaveisEstudantes AS
 CREATE VIEW listaPresenca AS
     SELECT p.Dia, p.fk_Estudante_CPF, p.Tipo FROM Presenca p;
 
-CREATE VIEW notificacoes AS
+CREATE VIEW notificacoes2 AS
     SELECT n.Data, n.Mensagem FROM Notificacoes n;
 
 -- criando papéis
@@ -69,32 +69,27 @@ GRANT UPDATE ON notificacoes TO funcionario;
 GRANT INSERT ON notificacoes TO funcionario;
 
 CREATE USER IF NOT EXISTS 'Admin'@'%' IDENTIFIED WITH mysql_native_password;
-SET old_passwords = 0;
-SET PASSWORD FOR 'Admin'@'%' = PASSWORD('admin');
-GRANT ALL PRIVILEGES ON *.* TO 'Admin'@'%' REQUIRE NONE WITH GRANT OPTION MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;
+SET PASSWORD FOR 'Admin'@'%' = 'admin';
+GRANT ALL PRIVILEGES ON *.* TO 'Admin'@'%';
 GRANT ALL PRIVILEGES ON `assiscole`.* TO 'Admin'@'%';
 
 CREATE USER IF NOT EXISTS 'Matheus'@'%' IDENTIFIED WITH mysql_native_password;
-SET old_passwords = 0;
-SET PASSWORD FOR 'Matheus'@'%' = PASSWORD('matheus');
-GRANT SELECT, INSERT, UPDATE ON *.* TO 'Matheus'@'%' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;
+SET PASSWORD FOR 'Matheus'@'%' = 'matheus';
+GRANT SELECT, INSERT, UPDATE ON *.* TO 'Matheus'@'%';
 GRANT ALL PRIVILEGES ON `assiscole`.* TO 'Matheus'@'%';
 
 CREATE USER IF NOT EXISTS 'Daniel'@'%' IDENTIFIED WITH mysql_native_password;
-SET old_passwords = 0;
-SET PASSWORD FOR 'Daniel'@'%' = PASSWORD('troll');
-GRANT SELECT, INSERT, UPDATE, DELETE, FILE ON *.* TO 'Daniel'@'%' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;
+SET PASSWORD FOR 'Daniel'@'%' = 'troll';
+GRANT SELECT, INSERT, UPDATE, DELETE, FILE ON *.* TO 'Daniel'@'%';
 GRANT ALL PRIVILEGES ON `assiscole`.* TO 'Daniel'@'%';
 
 CREATE USER IF NOT EXISTS 'funcionario_01'@'%' IDENTIFIED WITH mysql_native_password;
-SET old_passwords = 0;
-SET PASSWORD FOR 'funcionario_01'@'%' = PASSWORD('12345');
-GRANT USAGE ON *.* TO 'funcionario_01'@'%' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;
+SET PASSWORD FOR 'funcionario_01'@'%' = '12345';
+GRANT USAGE ON *.* TO 'funcionario_01'@'%';
 GRANT funcionario TO 'funcionario_01'@'%';
 
 CREATE USER IF NOT EXISTS 'professor_01'@'%' IDENTIFIED WITH mysql_native_password;
-SET old_passwords = 0;
-SET PASSWORD FOR 'professor_01'@'%' = PASSWORD('12345');
-GRANT USAGE ON *.* TO 'professor_01'@'%' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;
+SET PASSWORD FOR 'professor_01'@'%' = '12345';
+GRANT USAGE ON *.* TO 'professor_01'@'%';
 GRANT professor TO 'professor_01'@'%';
 
